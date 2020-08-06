ac25b62b1c12d8f4fea960a3b0fe613508d78a76
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-08-06 17:50:38 +0530

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,8 +1,10 @@
+#include "timer.h"
+
 void setupTimer(void){
 	//enabling output compare interrupt
 	TIMSK = (1<<OCIE1A);
 	//putting the value to compare counter with
-	OCR1A = 0x3D08;
+	OCR1A = OUTPUT_COMPARE;
 	//selecting clock, enabling CTC mode (Clear Timer on Compare) 
 	TCCR1B = (1<<CS12)|(1<<CS10)|(1<<WGM12);
 	count=0;
