39cea245f85e4553fec751a2a840c9e95523369a
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-08-22 12:13:14 +0530

    Update hmData.c

--- hmData.c
+++ hmData.c
@@ -30,6 +30,13 @@ void HMDataCheck(int freq, volatile uint8_t *hmData1, volatile int *count1, vola
 		{
 			//resetting the count variable and starting the i2c connection and sending the data to the EEPROM
 			*count1=0;
+			hmdata1 += 5;
+			if (DEPL1 == 1 && DEPL2 == 1) *hmdata1 = 0x33;
+			hmdata++;
+			if (VOLT1 == 1 && VOLT2 == 1) *hmdata1 = 0x33;
+			hmData1+=3;
+			if (CURR1 == 1 && CURR2 == 1) *hmdata1 = 0x33;
+			hmData1-=9;
 			i2cStart();
 			eepromWr();
 			address(0x00);
@@ -42,17 +49,17 @@ void HMDataCheck(int freq, volatile uint8_t *hmData1, volatile int *count1, vola
 			i2cStop();
 			}
       
-    else 
-    {
-      adcCheck(0,7,42,hmData1,opMode1);
-      adcCheck(1,8,42,hmData1,opMode1);
-      adcCheck(2,10,42,hmData1,opMode1);
-      adcCheck(3,11,42,hmData1,opMode1);
-      adcCheck(4,12,42,hmData1,opMode1);
-      adcCheck(5,13,42,hmData1,opMode1);
-      adcCheck(6,14,42,hmData1,opMode1);
-      adcCheck(7,15,42,hmData1,opMode1);
-    }
+    		else 
+    		{
+      			adcCheck(0,7,42,hmData1,opMode1);
+     			adcCheck(1,8,42,hmData1,opMode1);
+      			adcCheck(2,10,42,hmData1,opMode1);
+      			adcCheck(3,11,42,hmData1,opMode1);
+      			adcCheck(4,12,42,hmData1,opMode1);
+      			adcCheck(5,13,42,hmData1,opMode1);
+      			adcCheck(6,14,42,hmData1,opMode1);
+      			adcCheck(7,15,42,hmData1,opMode1);
+    			}
 		
 	}
 }
