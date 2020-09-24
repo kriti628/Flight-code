b67626d281c4bad139fc61a87a11aa421f1697b2
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-09-24 23:55:30 +0530

    Update hmData.c

--- hmData.c
+++ hmData.c
@@ -1,65 +1,60 @@
+#include "avr_lib.h"
 #include "hmData.h"
-#include "avr_compiler.h"
 #include "adc.h"
 #include "i2c.h"
 #include "spi.h"
 #include "usart.h"
 #include "timer.h"
 
-void HMDataCheck(int freq, volatile uint8_t *hmData1, volatile int *count1, volatile int *add1, volatile uint8_t *opMode1){
+extern uint8_t hmData[18];
+extern uint8_t opMode;
+extern int counter1;
+extern int counter2;
+extern uint8_t addr;
+
+void HMDataLoop(int freq1, int freq2){
 	//setting the pin status variables to their starting value
-	hmData1+=5;
-	*hmData1=0x00;
-	hmData1++;
-	*hmData1=0x00;
-	hmData1+=3;
-	*hmData1=0x00;
+	hmData[5]=0x00;
+	hmData[6]=0x00;
+	hmData[9]=0x00;
 	//setting up the timer with the required settings and initializing the various communication protocols used
-	setupTimer(count1);
-	adcInit();
-	uartInit();
-	spiInit();
-	i2cInit(add1);
-	//enabling the external interrupts and enabling global interrupts
-	hmData1-=9;
 	
-	while (*opMode1!=EMERGENCY)
-	{
-		//this is the code that gets executed every minute, and writes the HM data in the EEPROM
-		if (*count1==(freq/2))
-		{
+	//enabling the external interrupts and enabling global interrupts
+	
+	while(1){
+		if(counter1 == 2){
+			counter1 = 0;
+			
+			adcCheck(0x00,7,42);
+			adcCheck(0x01,8,42);
+			adcCheck(0x02,10,42);
+			adcCheck(0x03,11,42);
+			adcCheck(0x04,12,42);
+			adcCheck(0x05,13,42);
+			adcCheck(0x06,14,42);
+			adcCheck(0x07,15,42);
+			
+			counter2++ ;
+			PORTC = counter2;
+			_delay_ms(100);
+			PORTC = 0x00;
+			}
+			//this is the code that gets executed every minute, and writes the HM data in the EEPROM
+		if (counter2 == 5){	
 			//resetting the count variable and starting the i2c connection and sending the data to the EEPROM
-			*count1=0;
-			hmdata1 += 5;
-			if (PE0 == 1 && PE1 == 1) *hmdata1 = 0x33;
-			hmdata++;
-			if (PE2 == 1 && PE3 == 1) *hmdata1 = 0x33;
-			hmData1+=3;
-			if (PE4 == 1 && PE5 == 1) *hmdata1 = 0x33;
-			hmData1-=9;
+			counter2 = 0;
+			if (PE0 == 1 && PE1 == 1) hmData[5] = 0x33;
+			if (PE2 == 1 && PE3 == 1) hmData[6] = 0x33;
+			if (PE4 == 1 && PE5 == 1) hmData[9] = 0x33;
 			i2cStart();
 			eepromWr();
 			address(0x00);
-			address(*add1);
+			address(addr);
 			for (int i=0; i<18 ;i++){
-				eepromWrite(*hmData1,add1);
-				hmData1++;
-			}
-			hmData1-=17;
+				eepromWrite(hmData[i]);
+				}
 			i2cStop();
 			}
-      
-    		else 
-    		{
-      			adcCheck(0,7,42,hmData1,opMode1);
-     			adcCheck(1,8,42,hmData1,opMode1);
-      			adcCheck(2,10,42,hmData1,opMode1);
-      			adcCheck(3,11,42,hmData1,opMode1);
-      			adcCheck(4,12,42,hmData1,opMode1);
-      			adcCheck(5,13,42,hmData1,opMode1);
-      			adcCheck(6,14,42,hmData1,opMode1);
-      			adcCheck(7,15,42,hmData1,opMode1);
-    			}
 		
+		}
 	}
-}
