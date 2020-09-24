97e37f1c9d5babd1d25c6eaa3923cae981b42595
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-09-25 00:01:05 +0530

    Update timer.h

--- timer.h
+++ timer.h
@@ -2,8 +2,12 @@
 #define TIMER_H
 
 #include <stdint.h>
+#include "avr_lib.h"
 
-#define OUTPUT_COMPARE 0x3D08;
-void setupTimer(volatile uint8_t *count2);
+#define OUTPUT_COMPARE 0x3D09
+#define FREQ_FACTOR(OUTPUT_COMPARE, F_CPU) (1024/F_CPU)*OUTPUT_COMPARE
+
+
+void setupTimer(void);
 
 #endif
