23574198e43ebdec76a56f08731c933c86a04997
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-09-24 23:59:55 +0530

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,12 +1,13 @@
+#include "avr_lib.h"
 #include "timer.h"
-#include "avr_compiler.h"
 
-void setupTimer(volatile uint8_t *count2){
-	//enabling output compare interrupt
-	TIMSK = (1<<OCIE1A);
+void setupTimer(void){
+	//selecting clock, enabling CTC mode (Clear Timer on Compare)
+	TCCR1B = (1<<CS12)|(1<<CS10);
 	//putting the value to compare counter with
-	OCR1A = OUTPUT_COMPARE;
-	//selecting clock, enabling CTC mode (Clear Timer on Compare) 
-	TCCR1B = (1<<CS12)|(1<<CS10)|(1<<WGM12);
-	*count2=0;
+	OCR1C = OUTPUT_COMPARE;
+	//enabling output compare interrupt
+	ETIMSK = (1<<OCIE1C);
+	TCNT1 = 0;
+	
 }
