71db3d7578d504021bff8351f0238bdbfbbc51d2
kriti628 <60791427+kriti628@users.noreply.github.com>
2020-09-24 23:52:33 +0530

    Update adc.h

--- adc.h
+++ adc.h
@@ -1,9 +1,10 @@
-#ifndef ADC_H
-#define ADC_H
+  
+  #ifndef ADC_H
+  #define ADC_H
 
-#include <stdint.h>
+  #include <stdint.h>
 
-void adcInit(void);
-void adcCheck(uint8_t mux, int i, uint8_t idealValue, volatile uint8_t *hmData2, volatile uint8_t *opMode2);
+  void adcInit(void);
+  void adcCheck(uint8_t mux, int i, uint8_t idealValue);
 
-#endif
+  #endif
